cab0c9527195bc1f5c2de45b905fef9961c94cc8
Alexander Lindgren <alexander.lindgren@jayway.com>
2017-10-13 17:55:28 +0200

    Setup empty project

---
+++ Delta/Delta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delta
+//
+//  Created by Alexander Lindgren on 2017-10-13.
+//  Copyright © 2017 Alexander Lindgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
