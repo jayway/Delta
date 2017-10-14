f561a089af6ce484aa7817eaf2325e40c9e702c6
Alexander Lindgren <alexander.lindgren@jayway.com>
2017-10-14 09:49:47 +0200

    Setup viewcontroller2

--- Delta/Delta/ViewController.swift
+++ Delta/Delta/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        print("transition")
+        
+        
+    }
 
 }
 
